aa7c97e2105e124a7f81742398d38add7c8024c0
denis281542 <deniskuimov@mail.ru>
2019-11-07 22:41:18 +0500

    Edit layots

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :places
   resources :items
   get 'welcome/index'
   root 'items#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_193659) do
+ActiveRecord::Schema.define(version: 2019_11_06_185612) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
