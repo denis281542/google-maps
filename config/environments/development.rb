eb549c8ed52ee110d16bbb3ab8c0b2983ca2a8f5
denis281542 <deniskuimov@mail.ru>
2019-11-10 13:48:28 +0500

    Delete if else layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,19 +57,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:            "smtp.gmail.com",
-      port:               587,  
-      user_name:          'username@gmail.com',
-      password:           'password',
-      authentication:     "plain",
-      enable_starttls_auto: true
-  }
-    
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
