9cc4bda3de0b1a2e0e1e39581ed199ca9d51dc2e
denis281542 <deniskuimov@mail.ru>
2019-11-11 23:11:52 +0500

    Delete pay with card in layout

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,7 @@
-Stripe.api_key = "sk_test_Qc3WS4wKVnPWlj0znxCC1GhJ00Ubucy9Xb"
+Rails.configuration.stripe = {
+  :publishable_key => ENV['pk_test_PftUrNnWeOIvbCegQqhtS17r004JBuQgNY'],
+  :secret_key      => ENV['sk_test_Qc3WS4wKVnPWlj0znxCC1GhJ00Ubucy9Xb']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:sk_test_Qc3WS4wKVnPWlj0znxCC1GhJ00Ubucy9Xb]
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   
   resources :items
+
+  resources :charges
  
 end
   
