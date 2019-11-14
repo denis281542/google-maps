d031dbdbfec1b128d6bd014a40d84ddf1eec74e7
denis281542 <deniskuimov@mail.ru>
2019-11-14 18:23:35 +0500

    Added   image tag

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:title, :description, :price)
+      params.require(:item).permit(:title, :description, :price, :image)
     end
 end
