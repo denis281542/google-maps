7f3f45ea7eca7607050b1b369379e58144e7c6e1
denis281542 <deniskuimov@mail.ru>
2019-11-17 23:03:08 +0500

    Added search in model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
